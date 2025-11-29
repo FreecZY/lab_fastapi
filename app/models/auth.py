ce627af97bc0037038cebeb340a5f1d1819ad5f6
q <arsenshine@mail.ru>
2025-11-29 09:53:07 +0300

    del com

--- app/models/auth.py
+++ app/models/auth.py
@@ -12,31 +12,3 @@ class UserResponse(BaseModel):
     login: str
     login_time: datetime
     movies: list[Movie]
-
-
-
-
-"""class LoginRequest(BaseModel):
-    login: str
-    password: str
-
-
-class LoginResponse(BaseModel):
-    token: str
-    expires: datetime
-
-
-class SessionData(BaseModel):
-    expires: datetime
-    login: str
-
-
-class JWTPayload(BaseModel):
-    login_request: LoginRequest
-    exp: int
-
-
-class UserResponse(BaseModel):
-    session_data: SessionData
-    user_data: LoginRequest
-    movies: list[Movie]"""
